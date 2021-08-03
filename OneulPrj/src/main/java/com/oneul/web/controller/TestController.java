d46ba52f77fbf51669f91325de5e28931177a129
YOUNGHWA KIM <haileykim2014@gmail.com>
2021-08-03 22:46:00 +0900

    TestContoller

--- OneulPrj/src/main/java/com/oneul/web/controller/TestController.java
+++ OneulPrj/src/main/java/com/oneul/web/controller/TestController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.oneul.web.entity.CalendarEmotion;
@@ -33,7 +34,7 @@ import com.oneul.web.service.FreeDiaryService;
 import com.oneul.web.service.MemberService;
 import com.oneul.web.service.QuestionService;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class TestController {
 
